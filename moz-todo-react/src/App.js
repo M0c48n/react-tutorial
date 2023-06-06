cf5e17a96af5016c008acdd04749bcb5e216e60c
M0c48n <d41ki103@gmail.com>
2023-06-07 08:56:48 +0900

    TODOアプリに不要なファイル削除

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,14 +1,9 @@
-// import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App(props) {
   console.log(props);
   const subject = props.subject;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello, {subject}!
         </p>
