a05306b2ff0e8d64441a862f4fe799087e7f87f3
M0c48n <d41ki103@gmail.com>
2023-06-10 16:26:30 +0900

    データとしてのタスク

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,6 +1,15 @@
 import Todo from "./components/Todo";
 
 function App(props) {
+  const taskList = props.tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+  
     return (
         <div className="todoapp stack-large">
             <h1>TodoMatic</h1>
@@ -34,9 +43,7 @@ function App(props) {
             </div>
             <h2 id="list-heading">3 tasks remaining</h2>
             <ul role="list" className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
-                <Todo name="Eat" completed={true} id="todo-0" />
-                <Todo name="Sleep" completed={false} id="todo-1" />
-                <Todo name="Repeat" completed={false} id="todo-2" />
+                {taskList}
             </ul>
         </div>
     );
