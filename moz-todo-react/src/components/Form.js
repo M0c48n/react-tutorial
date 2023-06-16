e0b118971c84f8fb41e96cd56527019856c91c20
M0c48n <d41ki103@gmail.com>
2023-06-16 09:11:24 +0900

    Reading user input

--- moz-todo-react/src/components/Form.js
+++ moz-todo-react/src/components/Form.js
@@ -1,7 +1,8 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 function Form(props) {
+    const [name, setName] = useState("");
+
     function handleSubmit(e) {
         e.preventDefault();
         props.addTask("Say hello!");
@@ -13,7 +14,15 @@ function Form(props) {
                     What needs to be done?
                 </label>
             </h2>
-            <input type="text" id="new-todo-input" className="input input__lg" name="text" autoComplete="off" />
+            <input
+                type="text"
+                id="new-todo-input"
+                className="input input__lg"
+                name="text"
+                autoComplete="off"
+                value={name}
+                onChange={handleChange}
+            />
             <button type="submit" className="btn btn__primary btn__lg">
                 Add
             </button>
