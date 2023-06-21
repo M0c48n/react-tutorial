e79c534c502b9c184307b3c8f3a82c0254e354bc
M0c48n <d41ki103@gmail.com>
2023-06-21 22:35:05 +0900

    タスクを追加する機能作成

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
 function App(props) {
-    function addTask(name) {
-        alert(name);
-    }
+    const [tasks, setTasks] = useState(props.tasks);
 
-    const taskList = props.tasks.map((task) => (
+    function addTask(name) {
+        const newTask = { id: "id", name, completed: false };
+        setTasks([...tasks, newTask]);
+    }
+    const taskList = tasks.map((task) => (
         <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
     ));
     return (
