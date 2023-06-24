a86b5c7631530dd46c9a6c1451c45aae7ddadc3f
M0c48n <d41ki103@gmail.com>
2023-06-24 16:19:51 +0900

    counting tasks

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -14,6 +14,8 @@ function App(props) {
     const taskList = tasks.map((task) => (
         <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
     ));
+    const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+    const headingText = `${taskList.length} ${tasksNoun} remaining`;
     return (
         <div className="todoapp stack-large">
             <h1>TodoMatic</h1>
@@ -23,7 +25,7 @@ function App(props) {
                 <FilterButton />
                 <FilterButton />
             </div>
-            <h2 id="list-heading">3 tasks remaining</h2>
+            <h2 id="list-heading">{headingText}</h2>
             <ul role="list" className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
                 {taskList}
             </ul>
